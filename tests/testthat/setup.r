3c07f12b79b4da8bfaa54fef4862ec2d75c57785
Robert Zamora <zamora.rr@gmail.com>
2023-07-04 00:07:19 -0400

    minor update

--- tests/testthat/setup.r
+++ tests/testthat/setup.r
@@ -1,6 +1,8 @@
 library(keras)
 library(tensorflow)
-library(tfdatasets)
+#library(tfdatasets)
 
 Sys.setenv("XLA_FLAGS" = "--xla_gpu_cuda_data_dir=/usr/lib/cuda")
 reticulate::use_condaenv("r-reticulate")
+
+#option(tensorflow.extract.warn_tensors_passed_asis = FALSE)
